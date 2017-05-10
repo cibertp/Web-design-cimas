327d1b4b44b7dc0a716bd31e576f54e43fb6e81b
cibertp <cibertp462@gmail.com>
2017-05-10 13:18:15 -0500

    web store localstorage & sessionStorage

--- practicas nuevas/web storage/webStore.js
+++ practicas nuevas/web storage/webStore.js
@@ -1,19 +1,22 @@
-// JavaScript Document
+/* 
+	este caso los mismos son parecido la unica diferencia es que local store mantiene en el navegador temporalemente
+	no importa si cerraste el navegador siempre estara activa mientra que el otro solo es un dereminado teiimpo nada mas
+	//localStorage // sessionStorage
+*/
 
 
 
 function comenzar(){
 	var boton = document.getElementById("grabar");
-	boton.addEventListener("click",itemNuevo,false);
-	
+	boton.addEventListener("click",itemNuevo,false);	
 	
 }
 
 function itemNuevo(){
 	var clave= document.getElementById("clave").value;
 	var valor = document.getElementById("valor").value;
-	
-	sessionStorage.setItem(clave,valor);
+	localStorage
+	localStorage.setItem(clave,valor);
 	//sessionStorage[clave].valor;
 	leer_mostrar(clave);
 		var clave= document.getElementById("clave").value="";
@@ -24,28 +27,25 @@ function leer_mostrar(clave){
 	
 	var zonadatos= document.getElementById("zonadatos");
 	
-	
 	//zonadatos.innerHTML='<div><button type="button" onclick="Elimnartodo()" >Click Me!</button></div>';
 	zonadatos.innerHTML='<div><button  onclick="Elimnartodo()">Eliminar todo </button></div>';
 	
 		//var elvalor= sessionStorage[clave];
 	//zonadatos.innerHTML="";
 	
-	for(i=0;i<sessionStorage.length;i++){
-		var clave = sessionStorage.key(i);
-		var elvalor=sessionStorage.getItem(clave);
+	for(i=0;i<localStorage.length;i++){
+		var clave = localStorage.key(i);
+		var elvalor=localStorage.getItem(clave);
 		
 		//zonadatos.innerHTML+='<div>Clave : '+clave+ '__' +'Valor :'+ elvalor + '<br>';
 		zonadatos.innerHTML+='<div>Clave : '+clave+ '__' +'Valor :'+ elvalor + '<br><button onclick="eleminarItem(\''+ clave +'\')">Eliminar</botton></div>';
 	}
-	
-	
-	
+		
 }
 //todo bien
 function Elimnartodo(){
 		if(confirm("esta seguro?")){
-		   sessionStorage.clear();
+		   localStorage.clear();
 			leer_mostrar();
 		   }
 	
@@ -53,67 +53,9 @@ function Elimnartodo(){
 
 function eleminarItem(clave){
 	if(confirm("esta seguro")){
-		sessionStorage.removeItem(clave);
+		localStorage.removeItem(clave);
 		leer_mostrar();
 	}
 }
-
 
 window.addEventListener("load",comenzar,false);
-
-
-
-
-
-/*function comenzar (){
-	var boton = document.getElementById("grabar");
-	
-	boton.addEventListener("click",itemNuevo,false);
-}
-
-function itemNuevo(){
-	var clave = document.getElementById("clave").value;
-	var valor= document.getElementById("valor").value;
-	
-	sessionStorage.setItem(clave,valor);
-	//sessionStorage[clave]=valor;
-	
-	leer_mostrar(clave);
-	
-	document.getElementById("clave").value=""; 
-	document.getElementById("valor").value=""; 
-	
-}
-
-function leer_mostrar(clave){
-	var zonadatos = document.getElementById("zonadatos");
-
-	zonadatos.innerHTML='<div><button onclick=" eliminarTodo()">Eliminar todo+'<br></button></div>';
-	
-	//var elvalor=sessionStorage[clave];
-	//zonadatos.innerHTML="";
-	for(i=0;i<sessionStorage.length;i++){
-		var clave = sessionStorage.key(i);
-		var elvalor= sessionStorage.getItem(clave);
-	//zonadatos.innerHTML+="<div> clave :"+ clave + " __" +"el valor :"+ elvalor  + "</div>";
-	zonadatos.innerHTML+=' <div> clave : '+ clave  + ' __' +' el valor :'+ elvalor  + '<br><button onclick="eliminarItem(\''+ clave +'\')">Eliminar</button></div>';
-	
-	}
-	
-}
-
-
-function eliminarTodo(){
-	
-	if(confirm("estas seguro ?")) {
-		sessionStorage.clear();
-		leer_mostrar();
-	}
-}
-function eliminarItem(clave){
-	if(confirm("esta seguro?")
-	
-}
-z
-
-window.addEventListener("load",comenzar,false);*/
